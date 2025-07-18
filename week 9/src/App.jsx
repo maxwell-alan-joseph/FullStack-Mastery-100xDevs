72b026b386d189090ad3365c1b212d694e812463
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-07-18 12:32:35 +0530

    updating app.jsx

--- week 9/src/App.jsx
+++ week 9/src/App.jsx
@@ -1,10 +1,36 @@
-
 import './App.css'
+import miguel from './assets/miguel.jpg'
 
 function App() {
-
   return (
-    <h1>react basics</h1>
+    <div style = {{
+      background: "#1e272e",
+      height: "100vh",
+      }}>
+      <PostComponent />
+    </div>
+    
+  )
+}
+
+function PostComponent() {
+  return (
+    <div>
+      <div id="post1">
+        <img 
+        src={miguel} 
+        alt="miguel"
+        style={{
+          width: 30,
+          height: 30, 
+          borderRadius: 20
+        }}
+        />
+        <h1>Toji_</h1>
+        <h2>@binaryChamp</h2>
+    </div>
+    </div>
+    
   )
 }
 
