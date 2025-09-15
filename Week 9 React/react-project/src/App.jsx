3e9f741a5c499a837a9e94563b073e3ecbfa14cf
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-09-15 10:40:27 +0530

    notification counter

--- Week 9 React/react-project/src/App.jsx
+++ Week 9 React/react-project/src/App.jsx
@@ -1,7 +1,28 @@
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  
+  return (
+    <div>
+      <ToggleMessage />
+    </div>
+  );
 }
 
-export default App
+const ToggleMessage = () => {
+  let [notificationCount, setNotificationCount] = useState(0);
+
+  function increment() {
+    setNotificationCount(notificationCount + 2);
+  }
+
+  return (
+    <div>
+      <button onClick = {increment}>
+        Increase count + 2
+      </button>
+      {notificationCount}
+    </div>
+  );
+};
+
+export default App;
